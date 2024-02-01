5eeee258d585c8724ac4a39c74e0f3eae3c337a3
Ian Kinuthia 254 <144425644+KinuthiaIan@users.noreply.github.com>
2024-02-01 09:41:58 +0300

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,19 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 color="firebrick">Mburu is a web developer from Kenya</h1>
+      <main>
+     <Home />
+      <p>{name}</p>
+      <p>{city}</p>
+   </main>
+    </div>
+
+  );
 }
+
+
 
 export default Home;
